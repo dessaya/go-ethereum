2b2888a4c9c038e72102df673f18a0c07c217934
muXxer <mux3r@web.de>
2022-10-26 17:38:20 +0200

    Add ISC magic contract (#4)
    * feat: ISC contract
    
    * move ISCContract to vm.Config
    
    * add the option for isc contract to return an error
    
    * Revert cleanups to have less changes compared to origin
    
    Co-authored-by: Diego Essaya <dessaya@gmail.com>
    Co-authored-by: Jorge Silva <jorgesilva.me@gmail.com>

---
+++ core/vm/isc.go
@@ -0,0 +1,10 @@
+package vm
+
+import "github.com/ethereum/go-ethereum/common"
+
+// ISCAddress is the address of the special ISC system contract
+var ISCAddress = common.HexToAddress("0x1074")
+
+type ISCContract interface {
+	Run(evm *EVM, caller ContractRef, input []byte, gas uint64, readOnly bool) ([]byte, uint64, error)
+}
